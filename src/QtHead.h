5adf17fc0aadd5885c6402389a796c7cfb7f3359
327340773@qq.com <327340773@qq.com>
2016-01-11 10:45:59 +0800

    auto

--- src/QtHead.h
+++ src/QtHead.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #if defined(WIN32)
-//#ifdef Q_OS_WIN32
-
 #include "QtCore/QtCore"
 #include "QtGui/QtGui"
 #include "QtWebKit/QtWebKit"
 
-//#elif defined(Q_OS_MAC)
-
+#elif defined(MAC)
+#import <QtCore/QtCore>
+#import <QtGui/QtGui>
+#import <QtWebkit/QtWebkit>
 #endif
+
+//#ifdef Q_OS_WIN32
+//#elif defined(Q_OS_MAC)
